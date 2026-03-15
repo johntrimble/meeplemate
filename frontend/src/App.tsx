a6c963fe4f65e2008fe91c0f965a4362bfa480e5
John Trimble <trimblej@gmail.com>
2026-03-15 13:19:46 -0700

    Move auth provider to not include maintenance page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
+import { AuthProvider } from '@/auth/AuthProvider'
+import { TooltipProvider } from '@/components/ui/tooltip'
 import { ProtectedRoute } from '@/auth/ProtectedRoute'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
@@ -11,14 +13,22 @@ import MaintenancePage from './pages/MaintenancePage'
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/select-game" element={<SelectGamePage />} />
-      <Route path="/chat/:gameId" element={<ProtectedRoute><ChatPage /></ProtectedRoute>} />
-      <Route path="/chat/:gameId/:chatId" element={<ProtectedRoute><ChatPage /></ProtectedRoute>} />
-      <Route path="/not-authorized" element={<NotAuthorizedPage />} />
       <Route path="/maintenance" element={<MaintenancePage />} />
-      <Route path="*" element={<NotFoundPage />} />
+      <Route path="*" element={
+        <AuthProvider>
+          <TooltipProvider>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/select-game" element={<SelectGamePage />} />
+              <Route path="/chat/:gameId" element={<ProtectedRoute><ChatPage /></ProtectedRoute>} />
+              <Route path="/chat/:gameId/:chatId" element={<ProtectedRoute><ChatPage /></ProtectedRoute>} />
+              <Route path="/not-authorized" element={<NotAuthorizedPage />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </TooltipProvider>
+        </AuthProvider>
+      } />
     </Routes>
   )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { TooltipProvider } from '@/components/ui/tooltip'
-import { AuthProvider } from '@/auth/AuthProvider'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <TooltipProvider>
-          <App />
-        </TooltipProvider>
-      </AuthProvider>
+      <App />
     </BrowserRouter>
   </StrictMode>,
 )
