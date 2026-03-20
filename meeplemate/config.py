b4be0b9e864c0db466ded76403e121df05cec910
John Trimble <trimblej@gmail.com>
2026-03-19 20:32:50 -0700

    Fix postgres settings not passed to factory

--- meeplemate/config.py
+++ meeplemate/config.py
@@ -723,7 +723,9 @@ def create_app_system(cfg: Config) -> System[AppServices]:
                 factory(create_async_engine)(
                     cfg.pg.build_url(),
                     pool_size=cfg.pg.pool_size,
-                    max_overflow=cfg.pg.max_overflow
+                    max_overflow=cfg.pg.max_overflow,
+                    pool_pre_ping=cfg.pg.pool_pre_ping,
+                    pool_recycle=cfg.pg.pool_recycle,
                 ),
                 {}
             ),
